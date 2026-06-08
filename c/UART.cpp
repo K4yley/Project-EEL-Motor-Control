a096d8e44bfc00541afdef13428bc1a2ac68f184
Kayley Bollen <kayley.spikkel@gmail.com>
2026-06-08 13:01:45 +0200

    UART again

--- c/UART.cpp
+++ c/UART.cpp
@@ -4,9 +4,20 @@
 //then we waits till we get answers of not
 
 #define MAX_SIZE 64
-char tx_buff[MAX_SIZE];     //The buffer for the incomming data
 char rx_buff[MAX_SIZE];     //the buffer for the outgoing data
 int rx_i = 0;
+
+//Incomming Data
+    int I_Status;         //Set Position
+    int I_Value1;         //Set move forward; 0 or 1
+    int I_Value2;        //Set move backwards; 0 or 1
+    /*Expect: 
+        "Position, Forwards, backwards, Stop"
+        " 5, T, F, F"
+        " 10, F, T, F"
+        //State is everything about emerency; when true, stop all
+    */
+
 
 //Interrupt
 void on_uart_rx() {
@@ -16,9 +27,9 @@ void on_uart_rx() {
         if(input == '\n' || input == '\r'){
             if(rx_i > 0){
                 rx_buff[rx_i] = '\0';   
-                int result = sscanf(string, "%d, %d, %d", &I_Status, &I_Value2, &I_Value1);
+                int result = sscanf(rx_buff, "%d, %d, %d", &I_Status, &I_Value2, &I_Value1);
 
-                if(result == 7){
+                if(result == 3){
                     printf("Recieved: %d, %d, %d\n\n", &I_Status, &I_Value2, &I_Value1);
                 }
                 else{
@@ -45,19 +56,7 @@ int main() {
 
     uint32_t old_time = time_us_32();
     char tx_buff[MAX_SIZE];     //The buffer for the incomming data
-    char string[MAX_SIZE];
     
-    
-    //Incomming Data
-    int I_Status;         //Set Position
-    int I_Value1;         //Set move forward; 0 or 1
-    int I_Value2;        //Set move backwards; 0 or 1
-    /*Expect: 
-        "Position, Forwards, backwards, Stop"
-        " 5, T, F, F"
-        " 10, F, T, F"
-        //State is everything about emerency; when true, stop all
-    */
     
     //Outgoing Data
     float O_Voltage = 3.2;        //Live Voltage level
